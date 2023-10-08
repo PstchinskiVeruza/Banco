59105a757fff18840db9eb2771ded9b40bf8dba0
Pstchinski Veruza <125700404+PstchinskiVeruza@users.noreply.github.com>
2023-10-08 00:39:43 -0300

    Update titular.cpp

--- titular.cpp
+++ titular.cpp
@@ -1,23 +1,9 @@
 #include <string>
-#include <iostream>
 #include "titular.hpp"
+#include "pessoa.hpp"
+#include "autenticavel.hpp"
 
-Titular::Titular(std::string nome, Cpf cpf):
-	nome(nome), 
-	cpf(cpf)
-{
-	verificacaoNome();
-}
-
-std::string Titular::pegaNome() {
-	return nome;
-}
-
-void Titular::verificacaoNome() {
-	if (nome.size() < 5) {
-		std::cout << "Nome inválido" << std::endl;
-
-		exit(0);
-	}
-
-}
+Titular::Titular(std::string nome, Cpf cpf, std::string senha): 
+	Pessoa(nome, cpf),
+	Autenticavel(senha)
+{}

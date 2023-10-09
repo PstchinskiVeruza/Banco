bfa70b2c704f0548e7a895656179982d178d1128
Pstchinski Veruza <125700404+PstchinskiVeruza@users.noreply.github.com>
2023-10-09 01:42:21 -0300

    Update caixa.hpp

--- caixa.hpp
+++ caixa.hpp
@@ -3,10 +3,11 @@
 #include "funcionario.hpp"
 #include "autenticavel.hpp"
 #include "cpf.hpp"
+#include "diaSemana.hpp"
 
 class Caixa final : public Funcionario, public Autenticavel {
 public:
-	Caixa(std::string nome, Cpf cpf, float salario, std::string senha);
+	Caixa(std::string nome, Cpf cpf, float salario, DiaSemana diaPagamento, std::string senha);
 	float bonificacao() const override;
 
 };

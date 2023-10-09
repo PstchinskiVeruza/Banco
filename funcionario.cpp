3e5778bc3d1c9fe52fd1a79310e365e04db10b97
Pstchinski Veruza <125700404+PstchinskiVeruza@users.noreply.github.com>
2023-10-09 01:46:18 -0300

    Update funcionario.cpp

--- funcionario.cpp
+++ funcionario.cpp
@@ -2,15 +2,17 @@
 #include "funcionario.hpp"
 #include "pessoa.hpp"
 
-Funcionario::Funcionario(std::string nome, Cpf cpf, float salario): 
+Funcionario::Funcionario(std::string nome, Cpf cpf, float salario, DiaSemana diaPagamento):
 	Pessoa(nome, cpf), 
 	salario(salario)
 {}
 
 std::string Funcionario::pegaNome() const {
 	return nome;
+
 }
 
 float Funcionario::pegaSalario() const {
 	return salario;
+
 }

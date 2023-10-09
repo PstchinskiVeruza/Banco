25d2749e0c345add0b222a002eaf41b2212fcdce
Pstchinski Veruza <125700404+PstchinskiVeruza@users.noreply.github.com>
2023-10-09 01:47:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "funcionario.hpp"
 #include "gerente.hpp"
 #include "caixa.hpp"
+#include "diaSemana.hpp"
 
 using namespace std;
 
@@ -16,31 +17,51 @@ std::string autentico(Autenticavel& autenticando, std::string senha) {
 	}
 
 	return "Não identificado";
+
+}
+
+ostream& operator<<(ostream& cout, Conta& conta) {
+	Pessoa titular = conta.titular;
+	cout << conta.extrato() << endl;
+	cout << titular.pegaNome() << endl;
+	return cout;
+
+}
+
+template <typename variavel>
+variavel menor(variavel a, variavel b) {
+	return a < b ? a : b;
+
 }
 
 int main() {
 	Titular titular1("Pedro", Cpf("123.456.789-10"), "951413");
-	ContaPoupanca conta1("333", titular1);
+	ContaCorrente conta1("333", titular1);
 
 	Titular titular2("Rafael", Cpf("109.876.543-21"), "951413");
 	ContaCorrente conta2("666", titular2);
 
-	Gerente gerente("Jorge", Cpf("999.666.333-20"), 2400, "000000");
+	Gerente gerente("Jorge", Cpf("999.666.333-20"), 2400, DiaSemana::Terca, "000000");
 
-	Caixa caixa("Cleber", Cpf("333.666.999-36"), 1500, "999999");
+	Caixa caixa("Cleber", Cpf("333.666.999-36"), 1500, DiaSemana::Quarta,  "999999");
 
 	conta1.depositar(120);
 	conta2.depositar(120);
 
-	conta2.transferir(conta1, 10);
+	conta1 += conta2;
 
 	cout << caixa.pegaNome() << endl;
 
 	cout << Conta::pegaContasCriadas() << endl;
 
-	conta1.extrato();
-	conta2.extrato();
+	cout << conta1.extrato() << endl;
+	cout << conta2.extrato() << endl;
 
 	cout << autentico(titular1, "951413") << endl;
 
+	cout << conta1 << endl;
+
+	conta1.transferir(conta2, 10);
+
+	cout << menor<Conta&>(conta1, conta2) << endl;
 }

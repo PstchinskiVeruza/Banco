db83f32580f8a67a2a4f9418c879acd975abb634
Pstchinski Veruza <125700404+PstchinskiVeruza@users.noreply.github.com>
2023-10-09 01:45:57 -0300

    Update funcionario.hpp

--- funcionario.hpp
+++ funcionario.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include <string>
 #include "pessoa.hpp"
+#include "diaSemana.hpp"
 
 class Funcionario: public Pessoa {
 private:
 	float salario;
 
 public:
-	Funcionario(std::string nome, Cpf cpf, float salario);
+	Funcionario(std::string nome, Cpf cpf, float salario, DiaSemana diaPagamento);
 	std::string pegaNome() const;
 	float pegaSalario() const;
 	float virtual bonificacao() const = 0;

c388546f624e225f787d6aacf694821295b3c19c
Pstchinski Veruza <125700404+PstchinskiVeruza@users.noreply.github.com>
2023-10-09 01:46:47 -0300

    Update gerente.cpp

--- gerente.cpp
+++ gerente.cpp
@@ -3,11 +3,12 @@
 #include "funcionario.hpp"
 #include "autenticavel.hpp"
 
-Gerente::Gerente(std::string nome, Cpf cpf, float salario, std::string senha) :
-	Funcionario(nome, cpf, salario),
+Gerente::Gerente(std::string nome, Cpf cpf, float salario, DiaSemana diaPagamento, std::string senha) :
+	Funcionario(nome, cpf, salario, diaPagamento),
 	Autenticavel(senha)
 {}
 
 float Gerente::bonificacao() const {
 	return pegaSalario() * 0.15;
+
 }
